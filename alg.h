ed97c76809875e803ffc757f23a8ac2cc8937f5e
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2021-05-18 23:26:18 +0200

    added file alg_sparseVect.h

--- alg.h
+++ alg.h
@@ -16,6 +16,7 @@
 #include <numeric> // inner_product
 
 #include "alg_coeff.h"
+#include "alg_sparseVect.h"
 #include "alg_iter.h"
 
 /** \namespace alg
@@ -132,95 +133,6 @@ container for the sparse matrix coefficient, C.size() might be different from N,
 	std::vector<alg::m_coeff> C;
 }; // end class w_sparseMat
 
-/**
-\class sparseVect
-sparse vector : it is a container for v_coeff
-*/
-class sparseVect
-{
-public:
-	/** constructor */
-	inline sparseVect() {sorted = true;collected = false;}
-
-	/** inserter with value of a coefficient */
-	inline void push_back(const size_t idx,const double c) { x.push_back(alg::v_coeff(idx,c) ); sorted = false; collected=false; }
-
-	/** inserter with a coefficient */
-	inline void push_back(alg::v_coeff &coeff) { x.push_back(coeff); sorted = false; collected = false; }
-
-	/** sort the coefficients with lexicographic order */
-	inline void sort() {std::sort(x.begin(),x.end()); sorted = true;}
-
-	/** erase all coefficients with index idx */
-	inline void kill(const size_t idx) 
-		{x.erase(std::remove_if(x.begin(),x.end(),[this,&idx](alg::v_coeff &c) { return (c._i == idx); } ),x.end());}
-
-	/** erase all coefficients with value zero */
-	inline void kill_zero(void) 
-		{x.erase(std::remove_if(x.begin(),x.end(),[this](alg::v_coeff &c) { return (c.getVal() == 0); }),x.end() );}
-
-	/** collect method is sorting all v_coeffs, eventually with redundant indices, and is summing coeffs with same indices. It removes the coeffs that have been summed. */
-	inline void collect(void)
-		{
-		if (!sorted) sort();
-		std::vector<v_coeff>::iterator it = std::adjacent_find(x.begin(),x.end());
-		while (it != x.end())
-			{
-			alg::v_coeff c(it->_i,0);			
-			while((it != x.end()) && (c == *it)  )			
-				{ c.inc( it->getVal() ); ++it; }			
-			kill(c._i);			
-			push_back(c);			
-			it = std::adjacent_find(x.begin(),x.end());			
-			}		
-		collected = true;		
-		}
-
-	/** getter for sorted */
-	inline bool isSorted(void) const {return sorted;}
-	
-	/** getter for emptyness of the container of the coeffs */
-	inline bool isEmpty(void) const {return x.empty();} 
-
-	/** getter for collected */
-	inline bool isCollected(void) const {return collected;}
-
-	/** getter for the value of a coefficient of index idx, if several coeffs have the same index then it retruns the value of the first occurence */
-	inline double getVal(size_t idx) const
-		{
-		double val(0);
-		auto it = std::find_if(x.begin(),x.end(),[this,&idx](alg::v_coeff coeff){return (coeff._i == idx); } ); 
-		if (it != x.end()) val = it->getVal();		
-		return val;		
-		}
-
-	/** scalar product */
-	inline double p_scal(const std::vector<double> & X) const
-	{
-	double val(0);
-	if (!isCollected()) {std::cout << "warning : cannot p_scal on an uncollected sparseVect" << std::endl;exit(1);}
-	else
-		{const unsigned int X_dim = X.size();
-		for(auto it=x.begin();it!=x.end();++it)
-			{ if(it->_i < X_dim ) { val += it->getVal()*X[it->_i]; } }
-		}	
-	return val;
-	}
-
-	/** printing function */
-	inline void print(std::ostream & flux) const
-	{ flux<<'{'; std::for_each(x.begin(),x.end(), [&flux](const v_coeff &c){ flux << '{' << c._i << ':' << c.getVal() <<'}';}); flux<<"}\n"; }
-
-private:
-	/** coeffs container */
-std::vector< alg::v_coeff > x;
-
-	/** if true the coeffs are sorted */
-bool sorted; 
-
-/** if true the coeffs have been collected */
-bool collected;
-}; // end class sparseVect
 
 /** operator<< for w_sparseMat */
 inline std::ostream & operator<<(std::ostream & flux, w_sparseMat const& m) {m.print(flux); return flux;}
