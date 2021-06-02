96f28086c8ee4a8a98c9ba5902c9d2973ae13432
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2021-06-02 18:17:52 +0200

    unit test on mult sparseMat vect

--- tests/ut_sparseMat.cpp
+++ tests/ut_sparseMat.cpp
@@ -14,5 +14,35 @@ float x = 1.0;
 BOOST_CHECK(x != 0.0f);
 }
 
+BOOST_AUTO_TEST_CASE(w_sparseMat1)
+{
+alg::w_sparseMat m(5);
+m.push_back(1,1,3.14);
+m.push_back(1,1,1000);
+m.push_back(1,0,1);
+m.push_back(3,1,3.14);
+m.push_back(0,0,42);
+m.push_back(2,2,456);
+m.push_back(3,3,798);
+m.push_back(4,4,159);
+m.push_back(4,0,2.0);
+std::cout << "m=" << m << std::endl;
+
+alg::r_sparseMat r_m(m);
+r_m.print();
+BOOST_CHECK(m.getDim() == (size_t)5);
+
+std::vector<double> x = {0.5,0,0,0,1};
+std::vector<double> y;
+alg::mult(r_m,x,y);
+
+for(unsigned int i=0;i<y.size();i++) {std::cout << i <<':'<<y[i] <<" ; ";}
+
+BOOST_CHECK( y[0] == (double)21 );
+BOOST_CHECK( y[1] == (double)0.5 );
+BOOST_CHECK( y[2] == (double)0 );
+BOOST_CHECK( y[3] == (double)0 );
+BOOST_CHECK( y[4] == (double)160 );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/ut_sparseVect.cpp
+++ tests/ut_sparseVect.cpp
@@ -37,9 +37,6 @@ v.setVal(2,100.0);
 BOOST_CHECK(v.getVal(2) == (double)100.0);
 }
 
-
-
-
 BOOST_AUTO_TEST_CASE(sparseVect_kill_zero)
 {
 alg::sparseVect v;
@@ -129,30 +126,6 @@ std::cout << v << std::endl;
 double val = alg::p_scal(v,x);
 std::cout << "val (should be 1.57)=" << val << std::endl;
 BOOST_TEST(val == (double)1.57);
-}
-
-BOOST_AUTO_TEST_CASE(w_sparseMat1)
-{
-	alg::w_sparseMat m(5);
-	m.push_back(1,1,3.14);
-	m.push_back(1,1,1000);
-	m.push_back(1,0,1);
-	m.push_back(3,1,3.14);
-	m.push_back(0,0,42);
-	m.push_back(2,2,456);
-	m.push_back(3,3,798);
-	m.push_back(4,4,159);
-	m.push_back(4,0,2.0);
-	std::cout << "m=" << m << std::endl;
-
-	alg::r_sparseMat r_m(m);
-	r_m.print();
-	BOOST_CHECK(m.getDim() == (size_t)5);
-
-	std::vector<double> x = {0.5,0,0,0,1};
-	std::vector<double> y;
-	alg::mult(r_m,x,y);
-	for(unsigned int i=0;i<y.size();i++) {std::cout << i <<':'<<y[i] <<" ; ";}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
