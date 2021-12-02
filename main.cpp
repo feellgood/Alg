665dfd32a3a17f193d7c86c08c72220853b1ccdd
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2021-12-02 19:23:09 +0100

    gnuc version is checked

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ int main(int argc,char* argv[])
 Fem fem;
 std::string fileMsh;
 
+#if defined __GNUC__
+	std::cout << "gnuC detected version: " << __GNUC__ << "." << __GNUC_MINOR__ << std::endl;
+	#if ((__GNUC__ > 9)||((__GNUC__ == 9)&&(__GNUC_MINOR__ >= 3)) )
+		std::cout << "gnuc version Ok\n";
+	#else
+		std::cout << "This gnuc is too old to handle C++17.\n";
+	#endif
+#endif
+
 if (argc == 2)
 	{
 	fileMsh = argv[1];
