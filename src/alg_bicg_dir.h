4213a1b1cd2a1439dd26523e6c071d015ef66f89
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2021-12-15 22:20:30 +0100

    bicg_dir uses zeroFill and buildDiagPrecond functions

--- src/alg_bicg_dir.h
+++ src/alg_bicg_dir.h
@@ -15,20 +15,20 @@ std::vector<double> p(DIM), phat(DIM), shat(DIM), r(DIM), rt(DIM), s(DIM), t(DIM
 b.assign(rhs.begin(),rhs.end());// b = rhs;
 
 // le preconditionneur diagonal est une matrice diagonale contenant les inverses des coeffs de diag(A), ici on va stocker les coefficients dans un std::vector
-for(unsigned int i=0;i<diag_precond.size();i++)
-	{ diag_precond[i] = 1.0/A(i,i); }
+A.buildDiagPrecond(diag_precond);
 
 alg::mult(A, xd, v);
 alg::sub(v, b);      // b = b - A xd
 
-std::for_each(ld.begin(),ld.end(),[&b,&diag_precond](const size_t _i){ b[_i] = 0.0; diag_precond[_i] = 0.0; });
+zeroFill(ld,b);
+zeroFill(ld,diag_precond);
 iter.set_rhsnorm(alg::norm(b));
 	
 r.assign(b.begin(),b.end());// r = b;
 alg::mult(A, x, v);         // v = A x;
 alg::sub(v, r);             // r -= v; donc r = b - A x;
 
-std::for_each(ld.begin(),ld.end(),[&r](const size_t _i){ r[_i] = 0.0; });
+zeroFill(ld,r);
 rt.assign(r.begin(),r.end()); // copy(r, rt);
 p.assign(r.begin(),r.end()); // copy(r, p );
 
@@ -36,9 +36,6 @@ double tol=iter.get_resmax();
 std::cout << "tol : " << tol << std::endl;
 
 while (!iter.finished_vect(r)) {
-//while (alg::norm(r) > tol * alg::norm(b)) {
-      //std::cout << iter.get_iteration()  << "\t " << alg::norm(r) / alg::norm(b) << std::endl;
-
       rho_1 = alg::dot(rt,r);// rho_1 = vect_sp(rt, r);
       if (!iter.first()) { 
          beta = (rho_1 / rho_2) * (alpha / omega);
@@ -51,22 +48,19 @@ while (!iter.finished_vect(r)) {
 
       alg::p_direct(diag_precond, p, phat); // phat = M p;
       alg::mult(A, phat, v);                //  v = A phat;
-      std::for_each(ld.begin(),ld.end(),[&v](const size_t _i){ v[_i] = 0.0; }); // v[mask] = 0
-     
-	  alpha=rho_1/alg::dot(v, rt); // alpha = rho_1 /(v'*rtilde);
+      zeroFill(ld,v);
+      alpha=rho_1/alg::dot(v, rt); // alpha = rho_1 /(v'*rtilde);
       s.assign(r.begin(), r.end());   // s = r
-	  alg::scaled_add(v, -alpha, s);  // s = s -alpha v; donc s = r -alpha v
+	alg::scaled_add(v, -alpha, s);  // s = s -alpha v; donc s = r -alpha v
 
       if (iter.finished_vect(s)){
-     // if (alg::norm(s) < tol * alg::norm(b)) {
-	     alg::scaled_add(phat, alpha, x); // x = x + alpha phat
+ 	     alg::scaled_add(phat, alpha, x); // x = x + alpha phat
          break;
          }
 
       alg::p_direct(diag_precond, s, shat);// shat = M s;
       alg::mult(A, shat, t);               //  t = A shat;
-      std::for_each(ld.begin(),ld.end(),[&t](const size_t _i){ t[_i] = 0.0; }); // t[mask] = 0
-
+	zeroFill(ld,t);
       omega = alg::dot(t, s)/alg::dot(t,t); // omega = (t'* s) / (t'*t);
       alg::scaled_add(phat, alpha, x); // x = x + alpha phat;
       alg::scaled_add(shat, omega, x); // x = x + omega shat;
