20a6826d521d7aeb190bf8a3527009e39d2b675d
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2022-01-07 23:29:11 +0100

    scaled function implemented in GPU core.cu using lambda

--- src/GPU/core.cu
+++ src/GPU/core.cu
@@ -1,3 +1,5 @@
+#include<thrust/transform.h>
+#include<thrust/for_each.h>
 #include<thrust/inner_product.h>
 #include<thrust/functional.h>
 #include <thrust/host_vector.h>
@@ -6,8 +8,27 @@
 
 namespace GPU
 {
+
+/**  Y = alpha*X */
+void scaled(const thrust::device_vector<double> & X, const double alpha, thrust::device_vector<double> & Y) 
+	{ 
+	auto op = [alpha] __device__ (const double _x){ return alpha*_x; };
+	
+	thrust::transform(X.begin(),X.end(),Y.begin(), op ); 
+	}
+
+/**  Y *= alpha  */
+/*
+void scaled( const double alpha, thrust::device_vector<double> & Y) 
+	{ thrust::for_each(Y.begin(),Y.end(),[alpha](double &_x){ _x *= alpha; }); }
+*/
+
+/** return scalar product X.Y */
 double dot(const thrust::device_vector<double> & X,const thrust::device_vector<double> & Y)
 	{return thrust::inner_product(X.begin(),X.end(),Y.begin(),0.0); }
+
+
+
 }
 	
 int main(void)
@@ -31,6 +52,10 @@ std::cout <<"X[3] on host : " << X[3] << std::endl;
 std::cout <<"X[3] on device : " << dX[3] << std::endl;
 
 std::cout << "X.Y = " << GPU::dot(dX,dY) << std::endl;
+
+GPU::scaled(dX,2.0,dY);
+std::cout << "result = { " << dY[0] << "; " << dY[1] << "; "<< dY[2] << "; "<< dY[3] << "}" << std::endl;
+std::cout <<"CUDA error: " << cudaGetErrorString(cudaGetLastError()) << std::endl;
 
 return 0;
 }
