7ef3244ac32e0a7c5993257587dc24b7d1eb4493
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2022-04-02 23:51:14 +0200

    more cg

--- src/GPU/cg.h
+++ src/GPU/cg.h
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include <cusparse.h>
 
+#include<iostream>
 
 /* genTridiag: generate a random tridiagonal symmetric matrix */
 template <typename T>
@@ -31,11 +32,20 @@ void genTridiag(int *I, int *J, T *val, int N, int nz)
   I[N] = nz;
 }
 
+/*
+nz = nb de coefficients, stockés dans val, I,J indirections sur les indices de la matrices creuses x résultat ; rhs, second membre du système, de dimension N
+*/
+
 template <typename T>
-void cg(int *I, int *J, T *val, T *x, int N, int nz)
+void cg(int *I, int *J, T *val, T *x, T *rhs, int N, int nz, T tol, int max_iter)
 {
 int *d_col, *d_row;
-T *d_val, *d_x, *d_Ax;
+int k;
+T *d_val, *d_x, *d_r, *d_p, *d_Ax;
+T r0,r1,a,na,b,dot;
+
+cublasHandle_t cublasHandle = 0;
+cublasCreate(&cublasHandle);
 
 cusparseHandle_t cusparseHandle = 0;
 cusparseCreate(&cusparseHandle);
@@ -46,28 +56,35 @@ cudaMalloc((void **)&d_row, (N + 1) * sizeof(int));
 cudaMalloc((void **)&d_val, nz * sizeof(T));
 
 cudaMalloc((void **)&d_x, N * sizeof(T));
-//cudaMalloc((void **)&d_r, N * sizeof(float));
-//cudaMalloc((void **)&d_p, N * sizeof(float));
+cudaMalloc((void **)&d_r, N * sizeof(T));
+cudaMalloc((void **)&d_p, N * sizeof(T));
 cudaMalloc((void **)&d_Ax, N * sizeof(T));
 
   /* Wrap raw data into cuSPARSE generic API objects */
 cusparseSpMatDescr_t matA;
 cusparseCreateCsr(&matA, N, N, nz, d_row, d_col, d_val,CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F);
 
-cudaMemcpy(d_x,x,N*sizeof(float),cudaMemcpyHostToDevice);
+cudaMemcpy(d_x,x,N*sizeof(T),cudaMemcpyHostToDevice);
 
 cusparseDnVecDescr_t vecx;
 cusparseCreateDnVec(&vecx, N, d_x, CUDA_R_32F);
+
+cusparseDnVecDescr_t vecp;
+cusparseCreateDnVec(&vecp, N, d_p, CUDA_R_32F);
 
 cusparseDnVecDescr_t vecAx;
 cusparseCreateDnVec(&vecAx, N, d_Ax, CUDA_R_32F);
 
 cudaMemcpy(d_col, J, nz * sizeof(int), cudaMemcpyHostToDevice);
 cudaMemcpy(d_row, I, (N + 1) * sizeof(int), cudaMemcpyHostToDevice);
-cudaMemcpy(d_val, val, nz * sizeof(float), cudaMemcpyHostToDevice);
+cudaMemcpy(d_val, val, nz * sizeof(T), cudaMemcpyHostToDevice);
+cudaMemcpy(d_x,x,N*sizeof(T), cudaMemcpyHostToDevice);
+cudaMemcpy(d_r,rhs,N*sizeof(T), cudaMemcpyHostToDevice);
 
-float alpha = 1.0f;
-float beta = 0.0f;
+T alpha = 1.0f;
+T alpham1 = -1.0f;
+T beta = 0.0f;
+r0 = 0.0f;
 
 /* Allocate workspace for cuSPARSE */
 size_t bufferSize = 0;
@@ -77,11 +94,48 @@ cudaMalloc(&buffer, bufferSize);
 
 cusparseSpMV(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha, matA, vecx, &beta, vecAx, CUDA_R_32F,CUSPARSE_CSRMV_ALG1, buffer);
 
-cudaDeviceSynchronize();
+cublasSaxpy(cublasHandle, N, &alpham1, d_Ax, 1, d_r, 1);
+cublasSdot(cublasHandle, N, d_r , 1, d_r, 1, &r1);
 
-cudaMemcpy(x, d_Ax, N * sizeof(float), cudaMemcpyDeviceToHost);
+k=1;
+
+while(r1 > tol*tol && k <= max_iter)
+	{
+	if (k>1)
+		{
+		b = r1/r0;
+		cublasSscal(cublasHandle, N, &b, d_p, 1);
+		cublasSaxpy(cublasHandle, N, &alpha, d_r, 1, d_p, 1);
+		}
+	else
+		{ cublasScopy(cublasHandle, N, d_r, 1, d_p, 1); }
+	
+	cusparseSpMV(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha, matA, vecp, &beta, vecAx, CUDA_R_32F, CUSPARSE_CSRMV_ALG1, buffer);//CUSPARSE_SPMV_ALG_DEFAULT
+	
+	cublasSdot(cublasHandle, N, d_p, 1, d_Ax, 1, &dot);
+	a = r1/dot;
+	
+	cublasSaxpy(cublasHandle,N,&a,d_p,1,d_x,1);
+	na = -a;
+	cublasSaxpy(cublasHandle,N,&na,d_Ax,1,d_r,1);
+	
+	r0 = r1;
+	cublasSdot(cublasHandle,N,d_r,1,d_r,1,&r1);
+	cudaDeviceSynchronize();
+	std::cout << "iteration = " << k << " ; residual = " << sqrt(r1) << std::endl; 
+	k++;
+	}
+
+
+
+
+cudaMemcpy(x, d_x, N * sizeof(T), cudaMemcpyDeviceToHost);
+
+
+
 
 cusparseDestroy(cusparseHandle);
+cublasDestroy(cublasHandle);
 
 cusparseDestroySpMat(matA);
 cusparseDestroyDnVec(vecx);
@@ -91,5 +145,7 @@ cudaFree(d_col);
 cudaFree(d_row);
 cudaFree(d_val);
 cudaFree(d_x);
+cudaFree(d_r);
+cudaFree(d_p);
 cudaFree(d_Ax);
 }
--- src/GPU/test_cg.cu
+++ src/GPU/test_cg.cu
@@ -8,7 +8,7 @@ int main(void)
 const int N =10;
 int nz = (N-2)*3 + 4;
 int *I, *J ;
-float *val,  *x;
+float *val, *x, *rhs;
 
 I = new int[N+1];
 J = new int[nz];
@@ -18,13 +18,19 @@ genTridiag<float>(I, J, val, N, nz);
 //for(int i=0;i<nz;i++)	{std::cout<< "val[" << i << "]: " << val[i] <<std::endl;}
 
 x = new float[N];
+rhs = new float[N];
+
 for (int i = 0; i<N; i++)
 	{
 	x[i] = i/10.f;
+	rhs[i] = 1.0;
 	std::cout<< "x[" << i << "]: " << x[i] <<std::endl;
 	}
 
-cg<float>(I,J,val,x,N,nz);
+float tol = 1e-3;
+int max_iter = 100;
+
+cg<float>(I,J,val,x,rhs,N,nz,tol,max_iter);
 
 for(int i=0;i<N;i++)
 	{std::cout<< "x[" << i << "]: " << x[i] <<std::endl;}
@@ -33,6 +39,7 @@ delete[] x;
 delete[] I;
 delete[] J;
 delete[] val;
+delete[] rhs;
 
 std::cout <<"CUDA error: " << cudaGetErrorString(cudaGetLastError()) << std::endl;
 return 0;
