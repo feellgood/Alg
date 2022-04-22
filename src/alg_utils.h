ff3f0c3f18622161ae076f4bce97aafe9128d1e4
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2022-04-22 18:17:27 +0200

    utils to handle diagonal precond for CSR sparse matrix

--- src/alg_utils.h
+++ src/alg_utils.h
@@ -20,6 +20,34 @@ for(int i=0;i<N;i++)
 	}
 }
 
+/**
+build diagonal preconditionner of CSR matrix ; all diagonal coefficients must be non zero
+*/
+template <typename T>
+void build_diagPrecond_CSRmat(int *I,int *J, T *val, T *x,const int N, T * diag)
+{
+for(int i=0;i<N;i++)
+	{
+	for(int k=I[i];k<I[i+1];k++)
+		{ 
+		if ((J[k] == i)&&(val[k]!=0.0))
+			{ diag[k] = 1.0/val[k]; }
+		else
+			{ exit(1); }
+		}
+	}
+}
+
+
+/** in place left multiplication of a CSR sparse matrix by its diagonal preconditionner  ; CSR sparse matrix must not have any zero on the diagonal */
+template <typename T>
+void leftMult_diagPrecond_CSRmat(int *I,int *J, T *val, T *x,const int N, T * diag)
+{
+for(int i=0;i<N;i++)
+	for(int k=I[i];k<I[i+1];k++)
+		{ val[k] *= diag[i]; }
+}
+
 } // end namespace alg
 
 #endif
