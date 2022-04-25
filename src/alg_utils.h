a6d3e2bee0491d488a1df77cd2e86ec6c1ff19fa
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2022-04-25 16:30:56 +0200

    unit test for build sparse matrix precond diag

--- src/alg_utils.h
+++ src/alg_utils.h
@@ -22,24 +22,29 @@ for(int i=0;i<N;i++)
 
 /**
 build diagonal preconditionner of CSR matrix ; all diagonal coefficients must be non zero
+diagDim might be different from N
 */
 template <typename T>
-void build_diagPrecond_CSRmat(int *I,int *J, T *val, T *x,const int N, T * diag)
+void build_diagPrecond_CSRmat(int *I,int *J, T *val, T * diag,const int diagDim)
 {
-for(int i=0;i<N;i++)
+for(int i=0;i<diagDim;i++)
 	{
 	for(int k=I[i];k<I[i+1];k++)
 		{ 
-		if ((J[k] == i)&&(val[k]!=0.0))
-			{ diag[k] = 1.0/val[k]; }
-		else
-			{ exit(1); }
+		if (J[k] == i)
+			{
+			if (val[k]==0.0) { std::cout << "Error : zero coeff on the diagonal." << std::endl; exit(1); }
+			else
+				{ diag[i] = 1.0/val[k]; }
+			}
 		}
 	}
 }
 
 
-/** in place left multiplication of a CSR sparse matrix by its diagonal preconditionner  ; CSR sparse matrix must not have any zero on the diagonal */
+/** 
+in place left multiplication of a CSR sparse matrix by a diagonal matrix stored in diag
+ */
 template <typename T>
 void leftMult_diagPrecond_CSRmat(int *I,int *J, T *val, T *x,const int N, T * diag)
 {
